b596f12589aaf8bb0e3a721ce0c03354c090ac11
Artur Komarov <artur.komarov@gmail.com>
2015-02-12 15:21:26 +0300

    fixed token request

--- lib/omniauth/strategies/chronocity_oauth2.rb
+++ lib/omniauth/strategies/chronocity_oauth2.rb
@@ -7,7 +7,7 @@ module OmniAuth
 
       option :skip_friends, true
 
-      option :authorize_options, [:access_type, :hd, :login_hint, :prompt, :request_visible_actions, :state, :redirect_uri]
+      option :authorize_options, [:access_type, :hd, :login_hint, :prompt, :request_visible_actions, :redirect_uri]
 
       option :client_options, {
         :site          => 'http://liot.mipt.ru',
@@ -20,30 +20,18 @@ module OmniAuth
           options[:authorize_options].each do |k|
             params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
           end
-
-          session['omniauth.state'] = params[:state] if params['state']
         end
       end
 
       extra do
         hash = {}
-        hash[:id_token] = access_token['id_token']
+        hash[:info] = user_info(access_token.token)
         prune! hash
       end
 
-      def custom_build_access_token
-        if request.xhr? && request.params['code']
-          verifier = request.params['code']
-          client.auth_code.get_token(verifier, { :redirect_uri => 'postmessage'}.merge(token_params.to_hash(:symbolize_keys => true)),
-                                     deep_symbolize(options.auth_token_params || {}))
-        elsif verify_token(request.params['id_token'], request.params['access_token'])
-          ::OAuth2::AccessToken.from_hash(client, request.params.dup)
-        else
-          orig_build_access_token
-        end
+      uid do
+        user_info(access_token.token)['id']
       end
-      alias_method :orig_build_access_token, :build_access_token
-      alias_method :build_access_token, :custom_build_access_token
 
       private
 
@@ -54,31 +42,10 @@ module OmniAuth
         end
       end
 
-
-      def image_size_opts_passed?
-        !!(options[:image_size] || options[:image_aspect_ratio])
-      end
-
-      def image_params
-        image_params = []
-        if options[:image_size].is_a?(Integer)
-          image_params << "s#{options[:image_size]}"
-        elsif options[:image_size].is_a?(Hash)
-          image_params << "w#{options[:image_size][:width]}" if options[:image_size][:width]
-          image_params << "h#{options[:image_size][:height]}" if options[:image_size][:height]
-        end
-        image_params << 'c' if options[:image_aspect_ratio] == 'square'
-
-        '/' + image_params.join('-')
-      end
-
-      def verify_token(id_token, access_token)
-        return false unless (id_token && access_token)
-
+      def user_info(access_token)
         raw_response = client.request(:get, 'http://liot.mipt.ru/api/me', :params => {
           :access_token => access_token
         }).parsed
-        raw_response['issued_to'] == options.client_id
       end
     end
   end
